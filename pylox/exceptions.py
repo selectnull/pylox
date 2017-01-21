5d823173af3ad60d964018131c9edc67f27b8819
Sasha Matijasic <sasha@selectnull.com>
2017-01-21 21:28:49 +0100

    Add Return

--- pylox/exceptions.py
+++ pylox/exceptions.py
@@ -2,3 +2,8 @@ class RuntimeError(Exception):
     def __init__(self, token, message):
         super(message)
         self.token = token
+
+
+class Return(Exception):
+    def __init__(self, value):
+        self.value = value
